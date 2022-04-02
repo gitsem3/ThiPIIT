526a8ea14ca67f46ddd4a504818ebf77d9067a3f
viethoang0207 <71074038+0968377398@users.noreply.github.com>
2022-04-02 09:28:18 +0700

    nopbai

---
+++ ThiPIIT/Models/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThiPIIT.Models
+{
+    public class Coin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string BaseAsset { get; set; }
+        public string QuoteAsset { get; set; }
+        public string LastPrice { get; set; }
+        public string Volumn24h { get; set; }
+        public int MarketId { get; set; }
+        public DateTimeKind CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ ThiPIIT/Models/Market.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ThiPIIT.Models
+{
+    public class Market
+    {
+        [DisplayName("Id")]
+        [Required(ErrorMessage = "Please enter Id")]
+        public int Id { get; set; }
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Please enter Description")]
+        public string Description { get; set; }
+        [DisplayName("CreatAt")]
+        [Required(ErrorMessage = "Please enter CreateAt")]
+        public DateTime CreateAt { get; set; }
+        [DisplayName("UpdateAt")]
+        [Required(ErrorMessage = "Please enter UpdateAt")]
+        public DateTime UpdateAt { get; set; }
+        [DisplayName("Status")]
+        [Required(ErrorMessage = "Please enter Status")]
+        public int Status { get; set; }
+    }
+}
